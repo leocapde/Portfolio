6de508842219e9d4ec4068425f9906403e340337
Léo Capdevielle <leo.capde64@gmail.com>
2023-03-16 17:14:44 +0100

    Footer: envoi d'email depuis le formulaire (via l'api emailjs)

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,46 +1,102 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 export default function Footer() {
+  const formContact = useRef();
+  const sendEmail = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        formContact.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          if (window.confirm('Votre message a bien été envoyé !')) {
+            window.location.reload();
+          }
+        },
+        (error) => {
+          alert('Désolée une erreur est survenu...');
+          console.error(error.text);
+        }
+      );
+  };
+
   return (
     <section id="Footer" className="row bg-black pt-5 px-2 px-sm-5">
       <h4 className="text-light mb-0 mt-2">Contact</h4>
-      <form className="bg-black px-sm-5 mt-3" onSubmit={sendMessage}>
+      <form
+        className="bg-black px-sm-5 mt-3"
+        ref={formContact}
+        onSubmit={sendEmail}
+      >
         <div className="mb-3">
-          <label className="form-label text-light" htmlFor="email-contact-input">
-            Email
+          <label className="form-label text-light" htmlFor="name-contact-input">
+            Nom
           </label>
           <input
             className="form-control bg-dark text-light"
             type="text"
-            id="email-contact-input"
-            placeholder="votre@email.com"
+            id="name-contact-input"
+            name="user-name"
+            placeholder="Saul Goodman"
+            required
           />
         </div>
         <div className="mb-3">
-          <label className="form-label text-light" htmlFor="objet-contact-input">
+          <label
+            className="form-label text-light"
+            htmlFor="email-contact-input"
+          >
+            Email
+          </label>
+          <input
+            className="form-control bg-dark text-light"
+            type="email"
+            id="email-contact-input"
+            name="user-email"
+            placeholder="votre@email.com"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label
+            className="form-label text-light"
+            htmlFor="object-contact-input"
+          >
             Objet
           </label>
           <input
             className="form-control bg-dark text-light"
             type="text"
-            id="objet-contact-input"
+            id="object-contact-input"
+            name="user-object"
             placeholder="Hello there !"
+            required
           />
         </div>
         <div className="mb-3">
-          <label className="form-label text-light" htmlFor="message-contact-input">
+          <label
+            className="form-label text-light"
+            htmlFor="message-contact-input"
+          >
             Message
           </label>
           <textarea
             className="form-control bg-dark text-light"
             type="text"
             id="message-contact-input"
+            name="user-message"
             placeholder="Vous savez je ne crois pas qu'il y ait de bonne ou de mauvaise situation..."
+            required
           />
         </div>
         <div className="text-center">
-          <button
-            className="btn btn-secondary"
-            type="submit"
-          >
+          <button className="btn btn-secondary" type="submit">
             Envoyer
           </button>
         </div>
@@ -58,8 +114,4 @@ export default function Footer() {
       </div>
     </section>
   );
-}
-
-function sendMessage(event) {
-  window.alert('Votre message a bien été envoyé !');
 }
